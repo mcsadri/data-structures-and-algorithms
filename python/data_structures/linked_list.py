7f7e63212f229e4a869597470430cf707ea3e1f7
Manuch Sadri <colonquebang@hotmail.com>
2023-04-04 01:33:55 +0000

    initial commit for Linked List data structure

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -3,13 +3,29 @@ class LinkedList:
     Put docstring here
     """
 
-    def __init__(self):
+    def __init__(self, head=None):
         # initialization here
+        self.head = head
         pass
 
-    def some_method(self):
-        # method body here
+    def insert(self):
         pass
+
+    def includes(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    # def some_method(self):
+    #     # method body here
+    #     pass
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 
 class TargetError:

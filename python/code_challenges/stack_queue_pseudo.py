37426d370b9f82f9c8b01a006dfa1e1bb60cf93c
Manuch Sadri <colonquebang@hotmail.com>
2023-04-24 23:38:52 -0700

    basic code functionality for code challenge 11, PseudoQueue, working against provided unit tests

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,15 +1,26 @@
 from data_structures.stack import Stack
+from data_structures.invalid_operation_error import InvalidOperationError
 
 
 class PseudoQueue:
+    """
+    solution written with assistance from ChatGPT
+    """
     def __init__(self):
         self.stack_a = Stack()
         self.stack_b = Stack()
 
     def enqueue(self, value):
         # Inserts a value into the PseudoQueue, using a first-in, first-out approach.
-        pass
+        self.stack_a.push(value)
 
     def dequeue(self):
         # Extracts a value from the PseudoQueue, using a first-in, first-out approach.
-        pass
+        if self.stack_a.is_empty() and self.stack_b.is_empty():
+            raise InvalidOperationError(Exception("Method not allowed on empty collection"))
+
+        if self.stack_b.is_empty():
+            while not self.stack_a.is_empty():
+                self.stack_b.push(self.stack_a.pop())
+
+        return self.stack_b.pop()

81dfa8a8afea19d575c14ef28442b15a2c2b788e
Manuch Sadri <colonquebang@hotmail.com>
2023-04-24 23:23:58 -0700

    initial commit code challenge 11,  PseudoQueue

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,14 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack_a = Stack()
+        self.stack_b = Stack()
+
+    def enqueue(self, value):
+        # Inserts a value into the PseudoQueue, using a first-in, first-out approach.
+        pass
+
+    def dequeue(self):
+        # Extracts a value from the PseudoQueue, using a first-in, first-out approach.
+        pass

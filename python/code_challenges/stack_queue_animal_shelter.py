3fd17ecf23c5262fdf7bad24e695e9e703e1e3e2
Manuch Sadri <colonquebang@hotmail.com>
2023-04-27 23:00:20 -0700

    initial commit for cc12, stack-queue-animal-shelter. Basic, MVP code to pass provided unit tests

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,41 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    """
+    solution with assistance from ChatGPT
+    """
+    def __init__(self):
+        self.dogs = []
+        self.cats = []
+
+    def enqueue(self, animal):
+        if isinstance(animal, Dog):
+            self.dogs.append(animal)
+        elif isinstance(animal, Cat):
+            self.cats.append(animal)
+
+    def dequeue(self, pref):
+        if pref == 'dog':
+            if self.dogs:
+                return self.dogs.pop(0)
+            else:
+                return None
+        elif pref == 'cat':
+            if self.cats:
+                return self.cats.pop(0)
+            else:
+                return None
+        else:
+            return None
 
 
 class Dog:
-    pass
+    def __init__(self):
+        self.type = 'dog'
+        self.name = ''
 
 
 class Cat:
-    pass
+    def __init__(self):
+        self.type = 'cat'
+        self.name = ''

266257c188098303a37c1920c1bfc4359a5ff089
Manuch Sadri <colonquebang@hotmail.com>
2023-05-06 21:47:43 -0700

    initial commit Code Challenge 15

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -3,8 +3,21 @@ class BinaryTree:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
+    def __init__(self, root=None):
+        self.root = root
+
+    def pre_order(self, node):
+        results = []
+        results.append(self.value)
+
+        if node.left:
+            self.pre_order(node.left)
+
+        if node.right:
+            self.pre_order(node.right)
+
+    def some_method(self):
+        # method body here
         pass
 
     def some_method(self):
@@ -13,4 +26,30 @@ class BinaryTree:
 
 
 class Node:
-    pass
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+
+
+
+
+
+#
+# class BinaryTree:
+#     """
+#     Put docstring here
+#     """
+#
+#     def __init__(self):
+#         # initialization here
+#         pass
+#
+#     def some_method(self):
+#         # method body here
+#         pass
+#
+# class Node:
+#     pass

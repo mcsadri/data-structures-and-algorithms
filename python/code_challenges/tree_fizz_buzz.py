ce977f753eff863e536cb46c6a03f17259d92566
Manuch Sadri <colonquebang@hotmail.com>
2023-06-29 23:44:58 -0700

    starts whiteboarding process for code challenge 31

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,36 @@
-from data_structures.binary_tree import BinaryTree
+# from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree, Node
 
 
-def fizz_buzz_tree():
-    pass
+# attempted solution with assistance via ChatGPT
+def fizz_buzz_tree(tree):
+    def fizz_buzz(value):
+        if value % 3 == 0 and value % 5 == 0:
+            return "FizzBuzz"
+        elif value % 3 == 0:
+            return "Fizz"
+        elif value % 5 == 0:
+            return "Buzz"
+        else:
+            return str(value)
+
+    # def generate_tree(node):
+    #     value = node.value
+    #     fizz_buzz_value = fizz_buzz(value)
+    #     node.value = fizz_buzz_value
+    #
+    #     for i in range(tree.k):
+    #         child_value = value * tree.k + i + 1
+    #         child_node = tree.Node(child_value)
+    #         node.add_child(child_node)
+    #
+    #     for child in node.children:
+    #         generate_tree(child)
+    #
+    # root = tree.root
+    # generate_tree(root)
+    #
+    # return tree
+
+
+

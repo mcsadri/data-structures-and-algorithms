335e1aa33180b867f4db14b0ba7a10cb777eee8b
Manuch Sadri <colonquebang@hotmail.com>
2023-07-12 23:49:20 -0700

    adds demo code for add_node and add_edge helper functions

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -4,12 +4,17 @@ class Graph:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.graph = {}
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, node):
+        if node not in self.graph:
+            self.graph[node] = []
+
+    def add_edge(self, node1, node2, weight):
+        self.add_node(node1)
+        self.add_node(node2)
+        self.graph[node1].append([node2, weight])
+        self.graph[node2].append([node1, weight])
 
 
 class Vertex:
